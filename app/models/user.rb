a8f95bc29ef82cc48253ce41849220b76b1cb448
Christian TDN <cjwtdn@gmail.com>
2022-09-12 15:57:24 -0600

    updated controllers and routes, need to work on the authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-    has_many :liked_articles
-    has_many :articles, through: :liked_articles
+    # has_many :liked_articles
+    # has_many :articles, through: :liked_articles
+    validates :password, presence: true
+
 end
