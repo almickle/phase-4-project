7e844567644ac27b54186cc5a3d61cf4473bd1cc
almickle <105072712+almickle@users.noreply.github.com>
2022-09-11 18:47:08 -0600

    modules

--- client/frontend-native/App.js
+++ client/frontend-native/App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button, TextInput, View } from 'react-native';
+import { Formik } from 'formik';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Formik
+     initialValues={{ email: '' }}
+     onSubmit={values => console.log(values)}
+   >
+     {({ handleChange, handleBlur, handleSubmit, values }) => (
+       <View>
+         <TextInput
+           onChangeText={handleChange('email')}
+           onBlur={handleBlur('email')}
+           value={values.email}
+         />
+         <Button onPress={handleSubmit} title="Submit" />
+       </View>
+     )}
+   </Formik>
     </View>
   );
 }
