b859a58558a69d4b5b1f0913ffc7a70b143d4140
almickle <105072712+almickle@users.noreply.github.com>
2022-09-12 11:39:54 -0600

    initial components

--- client/frontend-native/App.js
+++ client/frontend-native/App.js
@@ -1,35 +1,16 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Button, TextInput, View } from 'react-native';
-import { Formik } from 'formik';
+import { View } from 'react-native';
+import Home from './components/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-       <Formik
-     initialValues={{ email: '' }}
-     onSubmit={values => console.log(values)}
-   >
-     {({ handleChange, handleBlur, handleSubmit, values }) => (
-       <View>
-         <TextInput
-           onChangeText={handleChange('email')}
-           onBlur={handleBlur('email')}
-           value={values.email}
-         />
-         <Button onPress={handleSubmit} title="Submit" />
-       </View>
-     )}
-   </Formik>
+    <View>
+        <Home />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
