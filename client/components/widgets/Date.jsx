7c8101962487f7b3d5feda7b0a60791c1239e1e9
almickle <105072712+almickle@users.noreply.github.com>
2022-09-13 19:59:02 -0600

    heroku

--- client/components/widgets/Date.jsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native';
-
-export default function Date() {
-    return (
-      <View>
-          
-      </View>
-    );
-  }
--- client/components/widgets/Markets.jsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native';
-
-export default function Markets() {
-    return (
-      <View>
-          
-      </View>
-    );
-  }
