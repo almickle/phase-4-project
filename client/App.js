3ceff734e2cb6a383244f884d6d2381cca1eaed5
gabrielb5 <104873216+gabrielb5@users.noreply.github.com>
2022-09-15 17:34:25 -0600

    working

--- client/App.js
+++ client/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
+// import React from 'react';
 import Home from './components/Home';
 import Form from './components/Form';
-
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { StyleSheet, Text, View, AppRegistry } from "react-native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Navigator from './components/homeStack'
+import { NavigationContainer } from '@react-navigation/native';
+
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Navigator from './components/homeStack'
+
+// import { NativeRouter, Route, Link, Routes } from "react-router-native";
+// import { Switch } from "react-router-dom";
 
 const Stack = createNativeStackNavigator();
+
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator >
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Form" path='/form' component={Form} />
+        <Stack.Screen name="Form" component={Form} />
       </Stack.Navigator>
-    </NavigationContainer>    
-  );
+    </NavigationContainer>
+);
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 25,
+    padding: 10
+  },
+  header: {
+    fontSize: 20
+  },
+  nav: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    // list-style-type: none,
+  margin: 0,
+  padding: 0,
+  },
+  navItem: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10
+  },
+  subNavItem: {
+    padding: 5
+  },
+  topic: {
+    textAlign: "center",
+    fontSize: 15
+  }
+});
+
+
 
 
 
